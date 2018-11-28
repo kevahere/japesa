9f091e333392c839c887758e6d4927e7c529bd42
kevahere <kevahere@gmail.com>
2018-11-28 12:40:40 +0300

    added the models and migrated

--- lending/models.py
+++ lending/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Lender(models.Model):
+    name = models.CharField(max_length=250)
+    category = models.CharField(max_length=250)
+    reviews = models.PositiveIntegerField(default=0)
+
+class Review(models.Model):
+    reviewer = models.CharField(max_length=250)
+    rating = models.PositiveIntegerField(default=0)
+    description = models.TextField(max_length=300)
+    likes = models.IntegerField(default=0)
+    pub_time = models.DateTimeField(auto_now_add=True)
